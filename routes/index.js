0b4c72b02b6c587c84140d14a06b1b1cb0b1106c
Guilherme Rovaron <Guilherme Rovaron>
2016-09-29 15:11:01 -0300

    route login

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 var express = require('express'),
+    jwt = require('jwt-simple'),
+    moment = require('moment'),
+    config = require('config'),
     router = express.Router();
 
 router.get('/', function (request, response){
@@ -12,7 +15,28 @@ router.get('/', function (request, response){
     }
 })
 
+router.post('/login', function(request, response, next){
+    var username = request.body.username;
+    var password = request.body.password;
+
+    if(username === 'Shawii' && password === '7488yn2w'){
+        var expires = moment().add(7, 'days').valueOf();
+        var token = jwt.encode({
+            user: username,
+            exp: expires
+        }, config.get('jwtTokenSecret'));
+
+        response.json({
+            token: token
+        })
+    } else {
+        var error = new Error('Unauthorized');
+        error.status = 401;
+        next(err);
+    }
+})
+
 //dogs
 router.use('/dogs', require('./dogs'));
-
 module.exports = router;
+
