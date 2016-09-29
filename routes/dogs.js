7d19faff184f6c4c6c387f8d321d3289d94c8275
Guilherme Rovaron <Guilherme Rovaron>
2016-09-29 17:51:02 -0300

    JWT dogs route

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,18 +1,40 @@
 var express = require('express'),
+    jwt = require('jwt-simple'),
+    config = require('config'),
+    moment = require('moment'),
     router = express.Router();
 
 var mongoose = require('../db/mongoose');
 var DogModel = require('../models/DogModel')(mongoose);
 var DogController = require('../controllers/DogController')(DogModel);
 
-var passport = require('passport');
+var middlewareAuth = function(request, response, next){
+    var token = request.query.token;
+    if(!token) {
+        var error = new Error('Forbidden');
+        error.status = 403;
+        return next(error);
+    }
+    try{
+        var decoded = jwt.decode(token, config.get('jwtTokenSecret'));
+        var irExpired = moment(decoded.exp).isBefore(new Date());
+        if(isExpired){
+            var error = new Error('Unauthorized');
+            error.status = 401;
+            return next(error);
+        } else {
+            request.user = decoded.user;
+            next();
+        }
+    } catch(error){
+        return next(error);
+    }
+}
 
 
-router.get('/', passport.authenticate('basic', { session: false }),
-DogController.getAll.bind(DogController));
+router.get('/', middlewareAuth, DogController.getAll.bind(DogController));
 
-router.get('/:_id', passport.authenticate('basic', { session: false }),
-DogController.getById.bind(DogController));
+router.get('/:_id', middlewareAuth, DogController.getById.bind(DogController));
 
 router.post('/', DogController.create.bind(DogController));
 
